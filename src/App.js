e3ec10b5694c5e5649469082423681a2e8ab248c
casscannelle <cassia.maffei@gmail.com>
2023-12-28 14:52:06 -0300

    ajustes calculo e mensagem do calculo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import './App.css'
+import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return(
-    <div className="container">
-      <div className="form">
-        <div className="container-input-label">
-          <div className="children-container-input-label">
-            <label>Altura:</label>
-            <input placeholder='Digite sua altura'/>
+function App () {
+
+  const [weight, setWeight] = useState()
+  const [height, setHeight] = useState()
+  const [imc, setImc] = useState('')
+  const [message, setMessage] = useState('')
+
+  function calculator () {
+    setImc((weight / ( height * height )).toFixed(2))
+    console.log(imc)
+
+    if (imc < 18.5) {
+      setMessage('Classificação: Magreza')
+    } else if (imc >= 18.6 && imc <= 24.9) {
+      setMessage('Classificação: Normal')
+    } else if (imc >=25 && imc<= 29.9){
+      setMessage('Classificação: Sobrepeso')
+    } else if (imc > 30){
+      setMessage('Classificação: Obesidade')
+    }
+  }
+
+  return (
+      <div className="IMC">
+          <div class="header"><h2>Descubra o seu IMC</h2></div>
+
+          <div class="form">
+          <label>Peso:</label>
+          <input 
+              type="text" 
+              placeholder="0.00" 
+              value={weight}
+              onChange={ e => setWeight(e.target.value)}
+          />
+          
+          <label>Altura: </label>
+          <input 
+              type="text" 
+              placeholder="0.00"
+              value={height} 
+              onChange={ e => setHeight(e.target.value)}
+          />
+          
+
+          <button onClick={calculator}>Calcular</button>
+
+          <p>Resultado: {imc}</p>
+          <p>{message}</p>
+          
           </div>
-          <div className="children-container-input-label">
-            <label>Peso:</label>
-            <input placeholder='Digite seu peso'/>
-          </div>
-          <div className="container-button">
-          <button>CALCULAR</button>
-        </div>
-        </div>
       </div>
-    </div>
   )
+
 }
 
 export default App
